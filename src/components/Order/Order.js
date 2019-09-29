2423c4564c757fa99808a310fe27bfdc8624c4d1
Mohammad Suhaib <mohammad.suhaib@edwisor.com>
2019-09-29 20:58:14 +0530

    burger order completed

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,35 @@
 import React from "react";
 import classes from "./Order.css";
 
-const Order = () => {
+const Order = props => {
+  const ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+
+  const ingredientOutput = ingredients.map(ig => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          border: "1px solid #ccc",
+          margin: "0 8px",
+          padding: "5px"
+        }}
+        key={ig.name}
+      >
+        {ig.name}({ig.amount})
+      </span>
+    );
+  });
   return (
     <div className={classes.Order}>
-      <p>Ingredients:</p>
-      <p>Price:</p>
+      <p>Ingredients:{ingredientOutput}</p>
+      <p>Price:{props.price.toFixed(2)}</p>
     </div>
   );
 };
